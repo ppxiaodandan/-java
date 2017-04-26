fa89b7c3ddfd3cc5327f6298ebeaebcde3b3ece6
ppxiaodandan <839212625@qq.com>
2017-04-26 10:45:45 +0800

    20170426-03

---
+++ 03/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;  
+ public class Main{
+  public static void main(String [] args)
+   {
+      Scanner scn=new Scanner(System.in);
+      while(scn.hasNext())
+      {
+      String str=scn.nextLine(); 
+      System.out.println(LowerUpperCase(str));
+       }
+       
+   }
+
+public static String LowerUpperCase(String s){//完成该函数功能，不需要写main()调用函数，系统会自动调用
+  
+  StringBuffer sb=new StringBuffer();
+  for(int i=0;i<s.length();i++)
+  {
+  if(s.charAt(i)>='a'&&s.charAt(i)<='z')
+  {
+    sb.append(Character.toUpperCase(s.charAt(i)));
+  }else if(s.charAt(i)>='A'&&s.charAt(i)<='Z')
+  {
+    sb.append(Character.toLowerCase(s.charAt(i)));
+  }else
+  {
+    sb.append(s.charAt(i));
+  }
+  }
+  return sb.toString();
+}
+
+}
